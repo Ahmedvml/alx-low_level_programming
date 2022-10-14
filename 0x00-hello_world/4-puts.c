89a82d77b525de71e7e57897126541b45f7cd6f1
Ahmed <Ahmedvml101@gmail.com>
2022-10-14 15:48:35 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash 
-#include <stdio.h> int puts(){write("Programming is like building a multilingual puzzle") return 0;}
+#include <stdio.h>
+/**
+* main - start function
+* description - "\"Programming is like building a multilingual puzzle"
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
