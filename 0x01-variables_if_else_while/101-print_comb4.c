68b08ce12707136f9ada10b8a53f1d3e04b7279f
Ahmed <Ahmedvml101@gmail.com>
2022-10-16 09:59:37 -0700

    Advanced task 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,36 @@
-/*
- * File: 100-print_comb3.c
- * Auth: Brennan D Baraban
- */
-
 #include <stdio.h>
-
 /**
- * main - Prints all possible combinations of two different digits,
- *        in ascending order, separated by a comma followed by a space.
- *
- * Return: Always 0.
+ * main-progrtam entry point
+ * Return:0 success, non zero fail
  */
 int main(void)
 {
-	int digit1, digit2;
+	int m, i, j;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		for (j = '0'; j <= '9'; j++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 8 && digit2 == 9)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
+			for (m = '0'; m <= '9'; m++)
+			{
+				if (i < j && j < m)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(m);
+					if (i == '7' && j == '8' && m == '9')
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
 	}
-
-	putchar('\n');
-
-	return (0);
+	putchar ('\n');
+				return (0);
 }
