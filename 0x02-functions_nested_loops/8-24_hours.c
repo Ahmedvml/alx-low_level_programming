ea99a9670cf4af06c50effb06ddd1b7ea8566dde
Ahmed <Ahmedvml101@gmail.com>
2022-10-19 10:56:14 -0700

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,10 @@
 #include "main.h"
-#include "_putchar.c"
+
 /**
- * jack_bauer-prints all minutes in a day from 00:00
+ * jack_bauer - prints all minutes in a day from 00:00
  *
  * Return:0 if succeesful.
  */
-
 
 void jack_bauer(void)
 {
