e55418bd4482b4976cfcf874fcdd0efbdab52663
Ahmed <Ahmedvml101@gmail.com>
2022-10-21 12:55:43 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "_putchar.c"
+
 /**
 *print_most_numbers - prints numbers except 2 and 4
 *
@@ -16,16 +16,4 @@ void print_most_numbers(void)
 			_putchar(c);
 	}
 	_putchar ('\n');
-}
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_most_numbers();
-    return (0);
 }
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*more_numbers - print 10 times the num from 0-14
+*
+*Return: void 
+*/
+
+void more_numbers(void)
+{
+	char c;
+	for (c ==
