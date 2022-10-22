e76b44bb35d92ddf5cb36e6444f3133527fd6d2b
Ahmed <Ahmedvml101@gmail.com>
2022-10-22 01:56:06 -0700

    Task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,13 +16,18 @@ if (a > b && a > c)
 {
 	largest = a;
 }
-else if (b > a && b > c)
+else if (b > c && b > a)
 {
 	largest = b;
 }
-else
+else if (c > b)
 {
 	largest = c;
+}
+
+else
+{
+	lagest = b;
 }
 
 return (largest);
