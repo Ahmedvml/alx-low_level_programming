169fc8405c2fb1888723ed02c20f197301d409b0
Ahmed <Ahmedvml101@gmail.com>
2022-10-23 01:12:57 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers between 0 to 9.
+ * Return: no return.
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (ch = 0; ch < 9; c++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
