c60f48073f198218e08cf3c558a935054d2ed40c
Ahmed <Ahmedvml101@gmail.com>
2022-10-23 01:40:03 -0700

    Task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <unistd.h>
 
+int _Putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
-void print_numbers(void);
+int print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
@@ -12,5 +12,4 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif
+int main(void);
