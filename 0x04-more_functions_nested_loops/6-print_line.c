ed6c3aa1c5af1ce80619a4e86d76a6353a3333d4
Ahmed <Ahmedvml101@gmail.com>
2022-10-23 02:08:38 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line- outputs '_' by the number of times specified by the
+ * user
+ * @n: the number of times '_' is to be outputted
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		if (n <= 0)
+		{
+			_putchar(10);
+		}
+		else
+		{
+			_putchar(95);
+		}
+	}
+	_putchar(10);
+}
