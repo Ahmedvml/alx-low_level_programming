f01ceb2f392b9371658f6fd274398d7363c16dce
Ahmed <Ahmedvml101@gmail.com>
2022-10-24 09:12:52 -0700

    Task 7

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,13 +13,13 @@ void print_diagonal(int n)
 
 	if (n <= 0)
 		_putchar('\n');
-	for (i = o; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
 			_putchar(' ');
 		}
-		_putchaer('\\');
+		_putchar('\\');
 		_putchar('\n');
 	}
 }
