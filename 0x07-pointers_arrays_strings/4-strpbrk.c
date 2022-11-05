ff126d126b3914b64ef60c20dd07f8dbd58102eb
Ahmed <Ahmedvml101@gmail.com>
2022-11-05 04:34:38 -0700

    Task 4.strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,10 +1,11 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  *_strpbrk-a function that searches a string for any of a set of bytes
  *
  *@s: occurence in the string
- *@accept: matches one of the bytes
+g*@accept: matches one of the bytes
  *@NULL: if no such byte
  *Return: a pointer to the byte
  */
