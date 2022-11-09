4286cc4063c958586358a530bea05bfe77b7bc4d
Ahmed <Ahmedvml101@gmail.com>
2022-11-09 07:12:50 -0800

    Task 1.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the programs
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always @ (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+(void) argv; 
+print("%i\n", argc - 1);
+
+  return 0;
+}
