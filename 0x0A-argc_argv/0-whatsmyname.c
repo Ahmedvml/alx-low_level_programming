59d6b72803b05e99290c5558aa3f201ab6be9076
Ahmed <Ahmedvml101@gmail.com>
2022-11-09 08:22:51 -0800

    Task 0.

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the name of the programs
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always @ (Success)
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%s\n", argv[0];
+  return 0;
+}
